2114bdfa3adc41321aeaa9956154b73b708b0c78
andre <andrelamurias@gmail.com>
2018-01-22 15:55:34 +0000

    Implement attention

--- src/models.py
+++ src/models.py
@@ -9,8 +9,14 @@ from keras.layers import Input
 from keras.layers import GlobalMaxPooling1D
 from keras.layers import Concatenate
 from keras.layers import Bidirectional
+from keras.layers import merge
+from keras.layers import Permute
+from keras.layers import Reshape
+from keras.layers import Lambda
+from keras.layers import RepeatVector
 from keras.optimizers import SGD
 from keras.optimizers import Adam
+from keras.optimizers import RMSprop
 from keras.preprocessing import sequence
 from keras import regularizers
 from keras import backend as K
@@ -26,21 +32,51 @@ sigmoid_l2_reg = 0.00001
 dropout1 = 0.5
 #n_classes = 19
 n_classes = 5
-max_sentence_length = 10
+max_sentence_length = 80
 max_ancestors_length = 10
 
 words_channel = True
 wordnet_channel = False
 ancestors_channel = False
 
+# https://github.com/philipperemy/keras-attention-mechanism
+def attention_3d_block(inputs):
+    # inputs.shape = (batch_size, time_steps, input_dim)
+    input_dim = int(inputs.shape[2])
+    a = Permute((2, 1))(inputs)
+    a = Reshape((input_dim, max_sentence_length))(a) # this line is not useful. It's just to know which dimension is what.
+    a = Dense(max_sentence_length, activation='softmax')(a)
+    #if SINGLE_ATTENTION_VECTOR:
+    a = Lambda(lambda x: K.mean(x, axis=1), name='dim_reduction')(a)
+    a = RepeatVector(input_dim)(a)
+    a_probs = Permute((2, 1), name='attention_vec')(a)
+    output_attention_mul = merge([inputs, a_probs], name='attention_mul', mode='mul')
+    return output_attention_mul
+
+
+def get_words_channel_single(words_input, embedding_matrix):
+    e_words = embedding_matrix
+    e_words = e_words(words_input)
+
+    #attention = attention_3d_block(e_words)
+
+    e_words = Dropout(dropout1)(e_words)
+    words_lstm = Bidirectional(LSTM(LSTM_units, input_shape=(max_sentence_length, embbed_size), return_sequences=True,
+                           kernel_regularizer=regularizers.l2(sigmoid_l2_reg)))(e_words)
+    words_lstm = Dropout(dropout1)(words_lstm)
+    words_pool = GlobalMaxPooling1D()(words_lstm)
+    return words_pool
+
+
 
 def get_words_channel_conc(words_input, embedding_matrix):
     concatenate = keras.layers.concatenate([words_input[0], words_input[1]], axis=-1)
     e_words = embedding_matrix
     e_words = e_words(concatenate)
-    e_words = Dropout(0.5)(e_words)
-    words_lstm = LSTM(LSTM_units, input_shape=(max_sentence_length, embbed_size), return_sequences=True,
-                           kernel_regularizer=regularizers.l2(sigmoid_l2_reg))(e_words)
+    #e_words = Dropout(dropout1)(e_words)
+
+    words_lstm = Bidirectional(LSTM(LSTM_units, input_shape=(max_sentence_length, embbed_size), return_sequences=True,
+                           kernel_regularizer=regularizers.l2(sigmoid_l2_reg)))(e_words)
     words_pool = GlobalMaxPooling1D()(words_lstm)
     return words_pool
 
@@ -54,7 +90,7 @@ def get_words_channel(words_input, embedding_matrix):
     e_words_left = embedding_matrix
     e_words_left = e_words_left(words_input[0])
 
-    e_words_left = Dropout(0.5)(e_words_left)
+    e_words_left = Dropout(dropout1)(e_words_left)
 
     # e_words_right = Embedding(len(embedding_matrix), embbed_size, input_length=max_sentence_length,
     #                    trainable=False)
@@ -63,12 +99,15 @@ def get_words_channel(words_input, embedding_matrix):
     e_words_right = embedding_matrix
     e_words_right = e_words_right(words_input[1])
 
-    e_words_right = Dropout(0.5)(e_words_right)
+    e_words_right = Dropout(dropout1)(e_words_right)
 
     words_lstm_left = LSTM(LSTM_units, input_shape=(max_sentence_length, embbed_size), return_sequences=True,
                            kernel_regularizer=regularizers.l2(sigmoid_l2_reg))(e_words_left)
+    words_lstm_left = Dropout(dropout1)(words_lstm_left)
+
     words_lstm_right = LSTM(LSTM_units, input_shape=(max_sentence_length, embbed_size), return_sequences=True,
                             kernel_regularizer=regularizers.l2(sigmoid_l2_reg))(e_words_right)
+    words_lstm_right = Dropout(dropout1)(words_lstm_right)
 
     words_pool_left = GlobalMaxPooling1D()(words_lstm_left)
     words_pool_right = GlobalMaxPooling1D()(words_lstm_right)
@@ -79,6 +118,36 @@ def get_words_channel(words_input, embedding_matrix):
     return (words_pool_left, words_pool_right)
 
 
+def get_chebi_channel(chebi_input, id_to_index):
+    e_ancestors_left = Embedding(len(id_to_index), chebi_embbed_size, input_length=max_ancestors_length,
+                                 trainable=True)
+    e_ancestors_left.build((None,))
+    e_ancestors_left = e_ancestors_left(chebi_input[0])
+
+    e_ancestors_left = Dropout(0.5)(e_ancestors_left)
+
+    e_ancestors_right = Embedding(len(id_to_index), chebi_embbed_size, input_length=max_ancestors_length,
+                                  trainable=True)
+    e_ancestors_right.build((None,))
+    # e_right.set_weights([embedding_matrix])
+    e_ancestors_right = e_ancestors_right(chebi_input[0])
+
+    e_ancestors_right = Dropout(0.5)(e_ancestors_right)
+
+    #ancestors_lstm_left = LSTM(LSTM_units, input_shape=(max_ancestors_length, chebi_embbed_size), return_sequences=True,
+    #                           kernel_regularizer=regularizers.l2(sigmoid_l2_reg))(e_ancestors_left)
+    #ancestors_lstm_right = LSTM(LSTM_units, input_shape=(max_ancestors_length, chebi_embbed_size),
+    #                            return_sequences=True,
+    #                            kernel_regularizer=regularizers.l2(sigmoid_l2_reg))(e_ancestors_right)
+
+
+    #ancestors_pool_left = GlobalMaxPooling1D()(ancestors_lstm_left)
+    #ancestors_pool_right = GlobalMaxPooling1D()(ancestors_lstm_right)
+
+    ancestors_dense_left = Dense(LSTM_units, input_shape=(max_ancestors_length, chebi_embbed_size))(e_ancestors_left)
+    ancestors_dense_right = Dense(LSTM_units, input_shape=(max_ancestors_length, chebi_embbed_size))(e_ancestors_right)
+    return ancestors_dense_left, ancestors_dense_right
+
 def get_model(embedding_matrix, wordnet_emb, id_to_index):
     inputs = []
     pool_layers = []
@@ -87,11 +156,16 @@ def get_model(embedding_matrix, wordnet_emb, id_to_index):
         words_input_right = Input(shape=(max_sentence_length,), name='right_words')
 
         inputs += [words_input_left, words_input_right]
+
+        #words_input = Input(shape=(max_sentence_length,), name='words')
+        #inputs += [words_input]
+
         words_pool_left, words_pool_right = get_words_channel((words_input_left, words_input_right),
                                                               embedding_matrix)
         pool_layers += [words_pool_left, words_pool_right]
         #words_pool = get_words_channel_conc((words_input_left, words_input_right),
         #                                                      embedding_matrix)
+        #words_pool = get_words_channel_single(words_input, embedding_matrix)
         #pool_layers += [words_pool]
 
     if wordnet_channel:
@@ -133,34 +207,11 @@ def get_model(embedding_matrix, wordnet_emb, id_to_index):
         # do not use LSTM - order is always the same
         ancestors_input_left = Input(shape=(max_ancestors_length,), name='left_ancestors')
         ancestors_input_right = Input(shape=(max_ancestors_length,), name='right_ancestors')
+        ancestors_common = Input(shape=(max_ancestors_length,), name='common_ancestors')
         inputs += [ancestors_input_left, ancestors_input_right]
 
-        e_ancestors_left = Embedding(len(id_to_index), chebi_embbed_size, input_length=max_ancestors_length,
-                                 trainable=True)
-        e_ancestors_left.build((None,))
-        e_ancestors_left = e_ancestors_left(ancestors_input_left)
-
-        e_ancestors_left = Dropout(0.5)(e_ancestors_left)
-
-        e_ancestors_right = Embedding(len(id_to_index), chebi_embbed_size, input_length=max_ancestors_length,
-                                  trainable=True)
-        e_ancestors_right.build((None,))
-        # e_right.set_weights([embedding_matrix])
-        e_ancestors_right = e_ancestors_right(ancestors_input_right)
-
-        e_ancestors_right = Dropout(0.5)(e_ancestors_right)
-
-        ancestors_lstm_left = LSTM(LSTM_units, input_shape=(max_ancestors_length, chebi_embbed_size), return_sequences=True,
-                               kernel_regularizer=regularizers.l2(sigmoid_l2_reg))(e_ancestors_left)
-        ancestors_lstm_right = LSTM(LSTM_units, input_shape=(max_ancestors_length, chebi_embbed_size), return_sequences=True,
-                                kernel_regularizer=regularizers.l2(sigmoid_l2_reg))(e_ancestors_right)
-
-        ancestors_pool_left = GlobalMaxPooling1D()(ancestors_lstm_left)
-        ancestors_pool_right = GlobalMaxPooling1D()(ancestors_lstm_right)
-
-        # we_hidden = Dense(sigmoid_units, activation='sigmoid',
-        #                  kernel_regularizer=regularizers.l2(sigmoid_l2_reg),)(concatenate)
-        # we_hidden = Dropout(dropout1)(we_hidden)
+        ancestors_pool_left, ancestors_pool_right = get_chebi_channel((ancestors_input_left, ancestors_input_right),
+                                                                      id_to_index)
         pool_layers += [ancestors_pool_left, ancestors_pool_right]
 
     if len(pool_layers) > 1:
@@ -180,6 +231,7 @@ def get_model(embedding_matrix, wordnet_emb, id_to_index):
     model.compile(loss='categorical_crossentropy',
                   optimizer=SGD(0.1),
                   #optimizer=Adam(),
+                  #optimizer=RMSprop(),
                   metrics=['accuracy', precision, recall, f1])
     print(model.summary())
     return model
